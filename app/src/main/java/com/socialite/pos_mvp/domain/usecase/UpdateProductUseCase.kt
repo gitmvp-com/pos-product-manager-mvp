7a929f20c48b2262337d2658837daaf230f13047
Fili <126945152+filiksyos@users.noreply.github.com>
2025-10-22 15:48:40 +0300

    Initialize POS Product Manager MVP with Jetpack Compose and Room Database

---
+++ app/src/main/java/com/socialite/pos_mvp/domain/usecase/UpdateProductUseCase.kt
@@ -0,0 +1,13 @@
+package com.socialite.pos_mvp.domain.usecase
+
+import com.socialite.pos_mvp.domain.model.Product
+import com.socialite.pos_mvp.domain.repository.ProductRepository
+import javax.inject.Inject
+
+class UpdateProductUseCase @Inject constructor(
+    private val repository: ProductRepository
+) {
+    suspend operator fun invoke(product: Product) {
+        repository.updateProduct(product)
+    }
+}
